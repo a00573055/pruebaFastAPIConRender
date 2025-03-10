b482ee1f54d611c1a8a4eba2d73156121e99abfc
a00573055 <a00573055@tec.mx>
2025-03-10 11:38:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
+from pydantic import BaseModel  
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
+
+class FormData(BaseModel):
+	primero: str
+	segundo: str
+	
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+	return {"message": "HelloWorld"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/suma")
+def suma(item: Annotated[FormData, Form()]):
+	return str(int(item.primero) + int(item.segundo))
+
+@app.post("/resta")
+def suma(item: Annotated[FormData, Form()]):
+	return str(int(item.primero) - int(item.segundo))
+
+@app.post("/multi")
+def suma(item: Annotated[FormData, Form()]):
+	return str(int(item.primero) * int(item.segundo))
+
+@app.post("/divi")
+def suma(item: Annotated[FormData, Form()]):
+	if (int(item.segundo) != 0):
+	    return str(int(item.primero) / int(item.segundo))
+	return "unable to divide by 0"
